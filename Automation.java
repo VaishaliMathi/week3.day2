fc3a6ddec8d3defbb6564178245ceaf90ec345b7
VaishaliMathi <112792215+VaishaliMathi@users.noreply.github.com>
2022-09-21 13:11:08 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,21 @@
+package week3.day2.oops;
+
+public class Automation extends MultiLanguage implements Language {
+
+	public static void main(String[] args) {
+	}
+	public void selenium() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	void ruby() {
+	}
+		// TODO Auto-generated method stub
+		
+	
+}
---
+++ Language.java
@@ -0,0 +1,5 @@
+package week3.day2.oops;
+
+public interface Language {
+
+}
